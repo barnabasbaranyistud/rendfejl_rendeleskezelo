1e80ba949368670fe551445aaed0e16d59482815
Barnabás Baranyi <barnabas.baranyi2@stud.uni-corvinus.hu>
2025-04-10 11:40:25 +0200

    Initial commit

--- Rendeleskezelo/MainForm.Designer.cs
+++ Rendeleskezelo/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Rendeleskezelo
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "MainForm";
+            Text = "Rendelések kezelése";
+            ResumeLayout(false);
         }
 
         #endregion
