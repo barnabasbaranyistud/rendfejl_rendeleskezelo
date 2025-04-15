b47220694df6236211fbaca20c3fff9ee1bebfc2
Barnabás Baranyi <barnabas.baranyi2@stud.uni-corvinus.hu>
2025-04-15 11:42:49 +0200

    Api call and Load Orders

--- Rendeleskezelo/MainForm.cs
+++ Rendeleskezelo/MainForm.cs
@@ -23,12 +23,31 @@ namespace Rendeleskezelo
         }
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            BetoltesRendelesek();
         }
 
-        private void LoadData()
+
+        private static Api ApiHivas()
         {
-            
-        }   
+            string url = "http://rendfejl10001.northeurope.cloudapp.azure.com:8080/DesktopModules/Hotcakes/API/rest/v1";
+            string kulcs = "1-7d286e89-c54f-430f-906e-f4ec7847b883"; // <-- Ide tedd a saját API kulcsodat
+            Api proxy = new Api(url, kulcs);
+            return proxy;
+        }
+
+        private void BetoltesRendelesek()
+        {
+            Api proxy = ApiHivas();
+
+            var response = proxy.OrdersFindAll();
+
+            if (response == null || response.Content == null || response.Content.Count == 0)
+            {
+                MessageBox.Show("Nem sikerült lekérni a rendeléseket vagy nincs adat.");
+                return;
+            }
+
+            dataGridViewOrders.DataSource = response.Content;
+        }
     }
 }
