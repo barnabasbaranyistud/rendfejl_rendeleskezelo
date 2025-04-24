e0a8aab4adad7d9966936597b807b39678513b9c
Barnabás Baranyi <barnabas.baranyi2@stud.uni-corvinus.hu>
2025-04-24 16:09:03 +0200

    Added nuget packages, an API controller for login testing

---
+++ LoginAPI/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using LoginAPI;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoginController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public LoginController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest model)
+    {
+        var user = _context.Users.SingleOrDefault(u => u.Username == model.Username);
+
+        if (user == null || !VerifyPasswordHash(model.Password, user.PasswordHash))
+        {
+            return Unauthorized("Invalid username or password.");
+        }
+
+        var token = GenerateJwtToken(user);
+        return Ok(new { Token = token });
+    }
+
+    private bool VerifyPasswordHash(string password, string storedHash)
+    {
+        // Ellenőrizzük, hogy a megadott jelszó megegyezik-e a tárolt hashelt jelszóval
+        // Használj megfelelő hash-ellenőrző algoritmusokat, mint bcrypt vagy SHA-256
+        return password == storedHash;  // Ide a saját hash ellenőrző kódodat írd!
+    }
+
+    private string GenerateJwtToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_secret_key_here"));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: "yourIssuer",
+            audience: "yourAudience",
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
+
--- Rendeleskezeles/LoginForm.cs
+++ Rendeleskezeles/LoginForm.cs
@@ -1,10 +1,13 @@
 ﻿using Hotcakes.CommerceDTO.v1.Client;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,22 +24,53 @@ namespace Rendeleskezelo
             this.StartPosition = FormStartPosition.CenterScreen;
         }
 
-        private void buttonLogin_Click(object sender, EventArgs e)
+        private async void buttonLogin_Click(object sender, EventArgs e)
         {
             string username = txtUsername.Text;
             string password = txtPassword.Text;
 
-            // Egyszerű hitelesítés
-            if (username == "a" && password == "a")
+            var loginSuccess = await LoginAsync(username, password);
+
+            if (loginSuccess)
             {
-                this.DialogResult = DialogResult.OK;
-                this.Close();
+                // Bejelentkezés sikeres, folytasd a munkát
+                MessageBox.Show("Login successful!");
             }
             else
             {
-                lblError.Text = "Hibás felhasználónév vagy jelszó!";
-                lblError.Visible = true;
+                // Bejelentkezés nem sikerült
+                MessageBox.Show("Login failed.");
             }
         }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            var loginRequest = new
+            {
+                Username = username,
+                Password = password
+            };
+
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(loginRequest), Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync("http://rendfejl10001.northeurope.cloudapp.azure.com:8080/api/login", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var token = JsonConvert.DeserializeObject<dynamic>(responseContent).Token;
+                    // Token elmentése, például a felhasználó munkamenetéhez
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password.");
+                    return false;
+                }
+            }
+        }
+
     }
 }
