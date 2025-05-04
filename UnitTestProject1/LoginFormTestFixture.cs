9ead3d09f0c4924174b6cfe14ef053492d134e4a
mdorka22 <mdorka22@gmail.com>
2025-05-04 21:33:07 +0200

    loginformtest

--- UnitTestProject1/LoginFormTestFixture.cs
+++ UnitTestProject1/LoginFormTestFixture.cs
@@ -1,14 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using Rendeleskezeles;
+﻿using NUnit.Framework;
+using Rendeleskezelo;
+using System.ComponentModel;
+using System.Configuration;
 
-namespace UnitTestProject1
+namespace Rendeleskezelo.Tests
 {
-    public class LoginFormTestFixture
+    [TestFixture]
+    public class LoginFormTests
     {
+        private LoginForm loginForm;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Set up a new instance of the form before each test
+            loginForm = new LoginForm();
+        }
+
+        [Test]
+        [TestCase("admin", "admin", DialogResult.OK)]  // Correct credentials
+        [TestCase("admin", "wrongpassword", DialogResult.Cancel)]  // Incorrect password
+        public void TestLoginForm(string username, string password, DialogResult expectedDialogResult)
+        {
+            // Arrange: Set the username and password
+            loginForm.txtUsername.Text = username;
+            loginForm.txtPassword.Text = password;
+
+            // Act: Simulate button click
+            loginForm.buttonLogin.PerformClick();
+
+            // Assert: Check if the dialog result matches the expected result
+            Assert.AreEqual(expectedDialogResult, loginForm.DialogResult);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up after each test
+            loginForm.Dispose();
+        }
     }
 }
